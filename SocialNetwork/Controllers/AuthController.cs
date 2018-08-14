db467008b293dc7bad2785a9ce8165419fb86b35
PFinnfors <pier@live.se>
2018-08-14 14:31:22 +0200

    First partial views and preparing account system

---
+++ SocialNetwork/Controllers/AuthController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SocialNetwork.Controllers
+{
+    public class AuthController : Controller
+    {
+        public IActionResult _login()
+        {
+            return PartialView();
+        }
+
+        public IActionResult _register()
+        {
+            return PartialView();
+        }
+    }
+}
--- SocialNetwork/wwwroot/js/site.js
+++ SocialNetwork/wwwroot/js/site.js
@@ -1,1 +1,29 @@
 ﻿// Write your JavaScript code.
+
+$(document).ready(function () {
+
+    $(".partial-contents").each(function (index, item) {
+
+        var url = $(item).data("url");
+        if (url && url.length > 0) {
+
+            $(item).load(url);
+        }
+    });
+    
+    
+});
+
+function load_register(elem) {
+    
+    var newUrl = $(elem).data("url");
+
+    $(elem).closest(".partial-contents").data("url", newUrl);
+
+    var url = $(elem).closest(".partial-contents").data("url");
+    
+    if (url && url.length > 0) {
+
+        $(elem).closest(".partial-contents").load(url);
+    }
+}
